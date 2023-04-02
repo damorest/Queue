9ec9f0173567437dbc35919b4f3ef97d7de7f632
damorest <pptreiding@gmail.com>
2023-04-02 23:50:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Queue
+{
+    internal class Program
+    {
+        class MyQueue
+        {
+            public List<string> data;
+            public MyQueue(List<string> initialData)
+            {
+                this.data = initialData;
+            }
+            public void Enqueue(string a)
+            {
+                this.data.Add(a);
+               /* Array.Resize(ref this.data, data.Length + 1);
+
+                string item = this.data[this.data.Length];*/
+            }
+
+        }
+        
+        static void Main(string[] args) {
+
+
+            List<string> arr = new List<string>()   {
+        "bird",
+        "frog",
+        "dog"
+    }; //{ "c", "num1", "num2" };
+           MyQueue test = new MyQueue(arr);
+            test.Enqueue("aaaaa");
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            /*MyQueue myQueue = new MyQueue(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                Console.WriteLine(myQueue.data[i]);
+            }*/
+
+        }
+    }
+}
---
+++ test1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Queue
+{
+    internal class Program
+    {
+        class MyQueue
+        {
+            public List<string> data;
+            public MyQueue(List<string> initialData)
+            {
+                this.data = initialData;
+            }
+            public void Enqueue(string a) // Додати елемент в кінець списку.
+            {
+                this.data.Add(a);
+            }
+            public void MyPeek(List<string> f) // Отримати перший елемент списку.
+            {
+                Console.WriteLine(f.First());
+
+                //return firstEl;
+            }
+            public void MyClear(List<string> fin) // Очищуємо чергу
+            {
+               fin.Clear();               
+            }
+            public void MyContains(string example) //Повертає true, якщо item є у списку.
+            {
+                foreach (var item in this.data)
+                {
+                    if (item == example)
+                    { 
+                        Console.WriteLine(true);
+                    }
+                }
+            }
+            public void Print(List<string> arr)
+            {
+                this.data = arr;
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            public void MyDequeue(List<string> arr) //Отримуємо і видаляємо перший елемент списку.
+            {
+                MyPeek(arr);
+                arr.RemoveAt(0);
+            }
+        }
+        
+        static void Main(string[] args) {
+
+
+            List<string> arr = new List<string>();
+            
+           MyQueue test = new MyQueue(arr);
+            test.Enqueue("cat");
+            test.Enqueue("cow");
+            test.Enqueue("bird");
+            test.Enqueue("dog");
+            test.Enqueue("volf");
+            
+            test.Print(arr);
+            test.MyDequeue(arr);
+            test.Print(arr);
+            //test.MyPeek(arr);
+            //test.MyContains("dog");
+          // test.MyClear(arr);
+
+           
+        }
+    }
+}
